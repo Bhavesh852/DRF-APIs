aada524c90918e5e38a72d618dbd2860a726808d
Bhavesh <bhavesh@samsride.com>
2023-03-18 10:54:41 +0530

    minor fix

--- Meta/settings.py
+++ Meta/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-6jozq7w-$0xgy0@^oq032h-%ev2akk5@g=tjw2_)5jw__!gkud
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
